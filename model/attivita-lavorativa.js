a9026819627503b53f1cb49f8f9d3fd8346a0c95
Alessio De Vincentis <alessiodevincentis95@gmail.com>
2023-05-13 09:50:58 +0200

    attivita lavorativa

--- model/attivita-lavorativa.js
+++ model/attivita-lavorativa.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 var schema = new mongoose.Schema({
     nome: {type: String},
     descrizione: {type: String},
+    titoloI: Boolean,
+    titoloIV: Boolean,
     dataInizioStimata: {type: Date},
     dataFineStimata: {type: Date},
-    idArea: {type: String},
-    idAziende: [{type: mongoose.Schema.Types.ObjectId}],
-    idDipendenti: [{type: String}]
+    idAree: [String],
+    idAziende: [String],
+    idDipendenti: [String],
+    idMezzi: [String]
 });
 
 const AttivitaLavorativaDb = mongoose.model('AttivitaLavorativaDb', schema, 'attivita_lavorativa');
