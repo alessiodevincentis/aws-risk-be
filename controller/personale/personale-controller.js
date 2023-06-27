a8e51216add3492659a9314efdb641f4c0866a64
Alessio De Vincentis <alessiodevincentis95@gmail.com>
2023-06-27 19:01:59 +0200

    filtri personale

--- controller/personale/personale-controller.js
+++ controller/personale/personale-controller.js
@@ -2,13 +2,39 @@ const PersonaleDb = require('../../model/personale.js')
 
 // retrieve and return all personale
 exports.find = (req, res)=>{
-    PersonaleDb.find()
+    let queryFilter = {$and: []};
+    if (req.query.tempoDeterminato) {
+        addFilterTempoDeterminato(queryFilter);
+    }
+    if (req.query.tempoIndeterminato) {
+        addFilterTempoIndeterminato(queryFilter);
+    }
+    if (req.query.idDitte && req.query.idDitte.split(',').length > 0) {
+        addFilterDitte(queryFilter,req.query.idDitte.split(','));
+    }
+    if (req.query.fattoriRischio && req.query.fattoriRischio.split(',').length > 0) {
+        addFilterFattoriRischio(queryFilter,req.query.fattoriRischio.split(','));
+    }
+    PersonaleDb.find(queryFilter.$and.length > 0 ? queryFilter : undefined)
         .then(personale => {
             res.send(personale)
         })
         .catch(err => {
             res.status(500).send({ message : err.message || "Error Occurred while retriving personale information" })
         })
+}
+
+function addFilterTempoIndeterminato(queryFilter) {
+    queryFilter.$and.push({"documentazione.documenti.infoUnilav.tipologiaContrattuale":"INDETERMINATO"})
+}
+function addFilterTempoDeterminato(queryFilter) {
+    queryFilter.$and.push({"documentazione.documenti.infoUnilav.tipologiaContrattuale":"DETERMINATO"})
+}
+function addFilterDitte(queryFilter,idDitte) {
+    queryFilter.$and.push({'anagrafica.idAzienda': {$in: idDitte}})
+}
+function addFilterFattoriRischio(queryFilter,fattoriRischio) {
+    queryFilter.$and.push({"documentazione.documenti.infoIdoneitaSanitaria.fattoriRischio": {$in: fattoriRischio}})
 }
 
 exports.insert = (req, res)=>{
