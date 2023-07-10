858353dc7ee3de0bd22b7f02d5663d37f27d144b
Alessio De Vincentis <alessiodevincentis95@gmail.com>
2023-07-10 09:04:26 +0200

    direct connection

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            family: 4
+            family: 4,
+            directConnection: true
         })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
