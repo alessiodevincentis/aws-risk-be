58632622b1a8e591172342d8fb06c70580b111fb
Alessio De Vincentis <alessiodevincentis95@gmail.com>
2023-09-11 10:19:44 +0200

    filtri mezzi

--- controller/mezzo/mezzo-controller.js
+++ controller/mezzo/mezzo-controller.js
@@ -2,13 +2,21 @@ const MezzoDb = require('../../model/mezzo.js')
 
 // retrieve and return all mezzi
 exports.find = (req, res)=>{
-    MezzoDb.find()
+    let queryFilter = {$and: []};
+    if (req.query.idDitte && req.query.idDitte.split(',').length > 0) {
+        addFilterDitte(queryFilter,req.query.idDitte.split(','));
+    }
+    MezzoDb.find(queryFilter.$and.length > 0 ? queryFilter : undefined)
         .then(mezzi => {
             res.send(mezzi)
         })
         .catch(err => {
             res.status(500).send({ message : err.message || "Error Occurred while retriving mezzi information" })
         })
+}
+
+function addFilterDitte(queryFilter,idDitte) {
+    queryFilter.$and.push({'anagrafica.idAzienda': {$in: idDitte}})
 }
 
 exports.insert = (req, res)=>{
